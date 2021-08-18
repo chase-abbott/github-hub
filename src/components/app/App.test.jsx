0d9a4f622c10f1872924efeb68ab5a1a737ce030
Chase <cabbott93@gmail.com>
2021-08-18 16:37:41 -0700

    passing snapshot

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,40 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import App from './App';
+import { ApolloProvider, createHttpLink, ApolloClient, InMemoryCache } from '@apollo/client';
+import { ContextProvider } from '../../state/ContextProvider';
+import { setContext } from '@apollo/client/link/context';
+
+const key = process.env.ACCESS_TOKEN;
+
+const link = createHttpLink({
+  uri: 'https://api.github.com/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${key}`
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(link),
+  cache: new InMemoryCache()
+});
 
 describe('App component', () => {
   afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<App />);
+    const { asFragment } = render(
+      <ApolloProvider client={client}>
+        <ContextProvider>
+          <App />
+        </ContextProvider>
+      </ApolloProvider>);
     expect(asFragment()).toMatchSnapshot();
   });
 });
